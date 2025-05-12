d1651b245c093231c159b382d4e9dba15071ed16
MrLohren <nachitofilm@gmail.com>
2025-05-11 21:23:09 -0400

    avance divs header

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="div-header">
+
+            <div className="div-profilepic"></div>
+
+        </div>
+    )
+}
+
+export default Header
